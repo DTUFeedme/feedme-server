d0c8d95f1a96c4a6497515780dfd2282d627fa4b
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-04-05 13:51:20 +0200

    deleted some logging

--- routes/feedback.js
+++ routes/feedback.js
@@ -152,8 +152,6 @@ router.patch("/change-answer/:feedbackId", [validateId, auth], async (req, res) 
     if (!feedback.question.answerOptions.find(e => e.id.toString() === req.body.answerId.toString()))
         return res.status(400).send("answerId was not included in question's answer options");
 
-
-    console.log(feedback);
     feedback.answer = req.body.answerId;
     const newFeedback = await feedback.save();
     res.send(newFeedback);
