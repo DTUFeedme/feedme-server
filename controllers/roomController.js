bd8479a402b1e0154bec563f426c566a03e045a4
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-04-07 08:00:46 +0200

    changed permission to get rooms from building temporarily

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,18 +1,18 @@
-const {Room, validate} = require("../models/room");
-const {Question} = require("../models/question");
-const {User} = require("../models/user");
-const {Feedback} = require("../models/feedback");
-const {Building} = require("../models/building");
-const {SignalMap} = require("../models/signalMap");
+const { Room, validate } = require("../models/room");
+const { Question } = require("../models/question");
+const { User } = require("../models/user");
+const { Feedback } = require("../models/feedback");
+const { Building } = require("../models/building");
+const { SignalMap } = require("../models/signalMap");
 
 const createRoom = async (req, res) => {
-    const {error} = validate(req.body);
+    const { error } = validate(req.body);
 
     if (error) return res.status(400).send(error.details[0].message);
 
-    const {name, location, buildingId} = req.body;
+    const { name, location, buildingId } = req.body;
 
-    if (await Building.countDocuments({_id: buildingId}) <= 0)
+    if (await Building.countDocuments({ _id: buildingId }) <= 0)
         return res.status(404).send('Building with id ' + buildingId + ' was not found.');
 
     let room = new Room({
@@ -26,9 +26,10 @@ const createRoom = async (req, res) => {
 };
 const getRoomsFromBuilding = async (req, res) => {
     const buildingId = req.params.id;
-    if (!req.user.adminOnBuildings.find(elem => elem.toString() === buildingId))
-        return res.status(403).send("User was not admin on building with id " + buildingId);
-    const rooms = await Room.find({building: buildingId});
+    // TODO: Should not be in a comment 
+    // if (!req.user.adminOnBuildings.find(elem => elem.toString() === buildingId))
+    // return res.status(403).send("User was not admin on building with id " + buildingId);
+    const rooms = await Room.find({ building: buildingId });
     res.send(rooms);
 };
 
@@ -41,7 +42,7 @@ const deleteRoom = async (req, res) => {
     if (!req.user.adminOnBuildings.find(elem => elem.toString() === room.building.toString()))
         return res.status(403).send("User needs to be admin on room to delete it");
 
-    const questions = await Question.find({rooms: room.id});
+    const questions = await Question.find({ rooms: room.id });
 
     for (let i = 0; i < questions.length; i++) {
         if (questions[i].rooms.length === 1) {
@@ -53,7 +54,7 @@ const deleteRoom = async (req, res) => {
             await questions[i].save();
         }
     }
-    const signalMaps = await SignalMap.find({room: room.id});
+    const signalMaps = await SignalMap.find({ room: room.id });
 
     for (let i = 0; i < signalMaps.length; i++) {
         await signalMaps[i].remove();
@@ -65,13 +66,13 @@ const deleteRoom = async (req, res) => {
 };
 
 const getRooms = async (req, res) => {
-    const {admin, feedback} = req.query;
+    const { admin, feedback } = req.query;
 
-    
+
     let rooms;
     if (admin) {
         if (admin === "me") {
-            rooms = await Room.find({building: {$in: req.user.adminOnBuildings}});
+            rooms = await Room.find({ building: { $in: req.user.adminOnBuildings } });
             console.log('rooms: ', rooms);
         } else {
             if (req.user.role < 2)
@@ -79,23 +80,23 @@ const getRooms = async (req, res) => {
             const user = await User.findById(admin);
             if (!user) return res.status(404).send(`User with id ${admin} was not found`);
 
-            rooms = await Room.find({building: {$in: user.adminOnBuildings}});
+            rooms = await Room.find({ building: { $in: user.adminOnBuildings } });
         }
 
     } else if (feedback) {
         if (feedback === "me") {
-            const feedback = await Feedback.find({user: req.user.id});
+            const feedback = await Feedback.find({ user: req.user.id });
             const roomsGivenFeedback = new Set();
             for (let i = 0; i < feedback.length; i++) {
                 roomsGivenFeedback.add(feedback[i].room);
             }
-            rooms = await Room.find({_id: {$in: Array.from(roomsGivenFeedback)}});
+            rooms = await Room.find({ _id: { $in: Array.from(roomsGivenFeedback) } });
         } else {
             return res.status(400).send("query feedback can only have value \"me\" ");
         }
 
     } else {
-        if (req.user.role < 2) 
+        if (req.user.role < 2)
             return res.status(403).send("User should have role admin to get all rooms");
         rooms = await Room.find();
     }
