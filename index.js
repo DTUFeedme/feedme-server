675c3592485534254568c05ba04e28dff2a72a21
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-04-23 23:14:22 +0200

    Updated and cleaned npm packages

--- index.js
+++ index.js
@@ -90,7 +90,6 @@ var payload = {
       console.log('Error sending message:', error);
     });*/
 
-
 // Logging
 if (process.env.NODE_ENV !== "production") {
     app.use(morgan("dev"));
@@ -128,7 +127,7 @@ if (!process.env.jwtPrivateKey) {
 if (process.env.NODE_ENV !== 'test') {
     app.listen(port, () => console.log(`Listening on port ${port}...`));
     const db = config.get('db');
-    mongoose.connect(db, { useNewUrlParser: true })
+    mongoose.connect(db, { useNewUrlParser: true , useUnifiedTopology: true})
         .then(() => console.log(`Connected to ${db}...`))
         .catch(err => console.log('Could not connect to MongoDB...', err));
 }
