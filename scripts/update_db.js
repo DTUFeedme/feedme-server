4af01d7055277fa45da146e36440e8361ec797ce
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-11-21 12:27:57 +0100

    Checking if beacon signals length are the same

--- scripts/update_db.js
+++ scripts/update_db.js
@@ -8,24 +8,11 @@ var buildings = db.buildings.find();
 var signalmaps = db.signalmaps.find();
 
 
-// buildings.forEach(b => {
-//     if (!b.admins)
-//         db.buildings.updateOne({_id: b._id}, {$set: {admins: []}});
-// });
-//
-// users.forEach(user => {
-//
-//     if (user.adminOnBuildings){
-//         user.adminOnBuildings.forEach(bId => {
-//             db.buildings.updateOne({_id: bId}, {$push: {admins: user._id}})
-//         });
-//         db.users.updateOne({_id: user._id}, {$unset: {adminOnBuildings: ""}});
-//     }
-// });
+
 // feedme on server: 5da41e00c525af695b69a72e
+
 buildings.forEach(b => {
     if (b._id.toString() !== "ObjectId(\"5da41e00c525af695b69a72e\")"){
-        // print(b.name);
         // db.buildings.remove({_id: b._id});
         print("removed building with name " + b.name);
         var rooms = db.rooms.find({building: b._id});
@@ -61,18 +48,42 @@ buildings.forEach(b => {
         });
         // print("hey");
     }
-
 });
+//
+// buildings.forEach(b => {
+//     if (!b.admins)
+//         db.buildings.updateOne({_id: b._id}, {$set: {admins: []}});
+// });
+//
+// users.forEach(user => {
+//
+//     if (user.adminOnBuildings){
+//         user.adminOnBuildings.forEach(bId => {
+//             db.buildings.updateOne({_id: bId}, {$push: {admins: user._id}})
+//         });
+//         db.users.updateOne({_id: user._id}, {$unset: {adminOnBuildings: ""}});
+//     }
+// });
+
 
 signalmaps.forEach(sm => {
 
     let signalLength = sm.beacons[0].signals.length;
     // print(sm.room);
+    // db.signalmaps.remove({_id: sm._id});
+
+    if (!sm.beacons || sm.beacons.length < 1)
+        print("WHAT" + sm._id);
 
     for (let i = 0; i < sm.beacons.length; i++) {
+        if (sm.beacons[i].signals.length !== signalLength){
+            // print("ERROR WITH SM " + sm._id);
+        } else {
+
+            // db.signalmaps.insert({room: sm.room, isActive: true, beacons: })
 
 
-        // if (sm.beacons[i].signals.length !== signalLength){
+        }
 
                 // let room = db.rooms.findOne({_id: sm.room});
                 // let buildings = db.buildings.findOne({_id: room.building});
