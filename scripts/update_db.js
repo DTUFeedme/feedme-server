cafd0e9839cc38f4c6a1eaf81e21101740d4ab70
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-11-21 11:55:33 +0100

    Checking if beacon signals length are the same

--- scripts/update_db.js
+++ scripts/update_db.js
@@ -25,15 +25,38 @@ var signalmaps = db.signalmaps.find();
 // feedme on server: 5da41e00c525af695b69a72e
 buildings.forEach(b => {
     if (b._id.toString() !== "ObjectId(\"5da41e00c525af695b69a72e\")"){
-        print(b.name);
+        // print(b.name);
         // db.buildings.remove({_id: b._id});
+        print("removed building with name " + b.name);
         var rooms = db.rooms.find({building: b._id});
+
         rooms.forEach(r => {
             // db.rooms.remove({_id: r._id});
+            print("removed room");
+
             var questions = db.questions.find({rooms: {$all: [r._id]}});
             questions.forEach(q => {
+                // db.questions.remove({_id: q._id});
+                print("removed question " + q.value);
+                q.answerOptions.forEach(ao => {
+                    // db.answers.remove({_id: ao._id});
+                    print("removed answer");
+                });
                 // printjson(q);
             });
+
+            var feedbacks = db.feedbacks.find({room: r._id});
+            feedbacks.forEach(f => {
+                // db.feedbacks.remove({_id: f._id});
+                print("removed feedback");
+            });
+
+            var signalmaps = db.signalmaps.find({room: r._id});
+            signalmaps.forEach(sm => {
+                // db.signalmaps.remove({_id: sm._id});
+                print("removed sm");
+            });
+
             // printjson(questions);
         });
         // print("hey");
