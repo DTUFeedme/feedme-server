eab1e811e710cf50201ca83f2d4322e2d02b205f
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-11-30 19:42:59 +0100

    Added update_beacon_names script

--- scripts/update_beacon_names.js
+++ scripts/update_beacon_names.js
@@ -11,7 +11,7 @@ beacons.forEach(b => {
     const newName = beaconNameMap[b.name];
 
     if (newName) {
-        db.beacons.updateOne({_id: b._id}, {name: newName});
+        db.beacons.update({_id: b._id}, {name: newName});
     } else {
         print("hmm");
     }
