e022df489dcb68ed2ea0f944724ff97c049b3206
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-11-30 20:30:36 +0100

    Added update_beacon_names script

--- scripts/update_db.js
+++ scripts/update_db.js
@@ -9,43 +9,43 @@ var signalmaps = db.signalmaps.find();
 
 // Removing all buildings except for 5da41e00c525af695b69a72e,
 // and all rooms, signalmaps, questions and feedback belonging to those buildings
-// buildings.forEach(b => {
-//     if (b._id.toString() !== "ObjectId(\"5da41e00c525af695b69a72e\")") {
-//     // if (b._id.toString() !== "ObjectId(\"5f37fcdae47d4e070d85aa83\")") {
-//
-//         db.buildings.remove({_id: b._id});
-//         print("removed building with name " + b.name);
-//         var rooms = db.rooms.find({building: b._id});
-//
-//         rooms.forEach(r => {
-//             db.rooms.remove({_id: r._id});
-//             print("removed room");
-//
-//             var questions = db.questions.find({rooms: {$all: [r._id]}});
-//             questions.forEach(q => {
-//                 db.questions.remove({_id: q._id});
-//                 print("removed question " + q.value);
-//
-//                 q.answerOptions.forEach(ao => {
-//                     db.answers.remove({_id: ao._id});
-//                     print("removed answer");
-//                 });
-//             });
-//
-//             var feedbacks = db.feedbacks.find({room: r._id});
-//             feedbacks.forEach(f => {
-//                 db.feedbacks.remove({_id: f._id});
-//                 print("removed feedback");
-//             });
-//
-//             var signalmaps = db.signalmaps.find({room: r._id});
-//             signalmaps.forEach(sm => {
-//                 db.signalmaps.remove({_id: sm._id});
-//                 print("removed sm");
-//             });
-//         });
-//     }
-// });
+buildings.forEach(b => {
+    if (b._id.toString() !== "ObjectId(\"5da41e00c525af695b69a72e\")") {
+    // if (b._id.toString() !== "ObjectId(\"5f37fcdae47d4e070d85aa83\")") {
+
+        db.buildings.remove({_id: b._id});
+        print("removed building with name " + b.name);
+        var rooms = db.rooms.find({building: b._id});
+
+        rooms.forEach(r => {
+            db.rooms.remove({_id: r._id});
+            print("removed room");
+
+            var questions = db.questions.find({rooms: {$all: [r._id]}});
+            questions.forEach(q => {
+                db.questions.remove({_id: q._id});
+                print("removed question " + q.value);
+
+                q.answerOptions.forEach(ao => {
+                    db.answers.remove({_id: ao._id});
+                    print("removed answer");
+                });
+            });
+
+            var feedbacks = db.feedbacks.find({room: r._id});
+            feedbacks.forEach(f => {
+                db.feedbacks.remove({_id: f._id});
+                print("removed feedback");
+            });
+
+            var signalmaps = db.signalmaps.find({room: r._id});
+            signalmaps.forEach(sm => {
+                db.signalmaps.remove({_id: sm._id});
+                print("removed sm");
+            });
+        });
+    }
+});
 //
 // // Updating all buildings without admins array with empty array.
 // buildings.forEach(b => {
