88c3701bd901433f8a3021512be4f98e89e7075b
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-12-01 08:39:30 +0100

    Added update_beacon_names script

--- scripts/update_db.js
+++ scripts/update_db.js
@@ -9,43 +9,43 @@ var signalmaps = db.signalmaps.find();
 
 // Removing all buildings except for 5da41e00c525af695b69a72e,
 // and all rooms, signalmaps, questions and feedback belonging to those buildings
-buildings.forEach(b => {
-    if (b._id.toString() !== "ObjectId(\"5da41e00c525af695b69a72e\")") {
-    // if (b._id.toString() !== "ObjectId(\"5f37fcdae47d4e070d85aa83\")") {
-
-        db.buildings.remove({_id: b._id});
-        print("removed building with name " + b.name);
-        var rooms = db.rooms.find({building: b._id});
-
-        rooms.forEach(r => {
-            db.rooms.remove({_id: r._id});
-            print("removed room");
-
-            var questions = db.questions.find({rooms: {$all: [r._id]}});
-            questions.forEach(q => {
-                db.questions.remove({_id: q._id});
-                print("removed question " + q.value);
-
-                q.answerOptions.forEach(ao => {
-                    db.answers.remove({_id: ao._id});
-                    print("removed answer");
-                });
-            });
-
-            var feedbacks = db.feedbacks.find({room: r._id});
-            feedbacks.forEach(f => {
-                db.feedbacks.remove({_id: f._id});
-                print("removed feedback");
-            });
-
-            var signalmaps = db.signalmaps.find({room: r._id});
-            signalmaps.forEach(sm => {
-                db.signalmaps.remove({_id: sm._id});
-                print("removed sm");
-            });
-        });
-    }
-});
+// buildings.forEach(b => {
+//     if (b._id.toString() !== "ObjectId(\"5da41e00c525af695b69a72e\")") {
+//     // if (b._id.toString() !== "ObjectId(\"5f37fcdae47d4e070d85aa83\")") {
+//
+//         db.buildings.remove({_id: b._id});
+//         print("removed building with name " + b.name);
+//         var rooms = db.rooms.find({building: b._id});
+//
+//         rooms.forEach(r => {
+//             db.rooms.remove({_id: r._id});
+//             print("removed room");
+//
+//             var questions = db.questions.find({rooms: {$all: [r._id]}});
+//             questions.forEach(q => {
+//                 db.questions.remove({_id: q._id});
+//                 print("removed question " + q.value);
+//
+//                 q.answerOptions.forEach(ao => {
+//                     db.answers.remove({_id: ao._id});
+//                     print("removed answer");
+//                 });
+//             });
+//
+//             var feedbacks = db.feedbacks.find({room: r._id});
+//             feedbacks.forEach(f => {
+//                 db.feedbacks.remove({_id: f._id});
+//                 print("removed feedback");
+//             });
+//
+//             var signalmaps = db.signalmaps.find({room: r._id});
+//             signalmaps.forEach(sm => {
+//                 db.signalmaps.remove({_id: sm._id});
+//                 print("removed sm");
+//             });
+//         });
+//     }
+// });
 //
 // Updating all buildings without admins array with empty array.
 // buildings.forEach(b => {
@@ -64,54 +64,54 @@ buildings.forEach(b => {
 // });
 
 // Convert signalmap with array of signals to individual signalmaps
-// signalmaps = db.signalmaps.find();
-// let smInserted = 0;
-// let smRemoved = 0;
-// signalmaps.forEach(sm => {
-//     let signalLength = sm.beacons[0].signals.length;
-//     print("removed sm " + smRemoved);
-//     smRemoved++;
-//     db.signalmaps.remove({_id: sm._id});
-//
-//
-//     for (let i = 0; i < sm.beacons[0].signals.length; i++) {
-//         var newSm = {
-//             isActive: true,
-//             room: sm.room,
-//             beacons: []
-//         }
-//
-//         for (let j = 0; j < sm.beacons.length; j++) {
-//
-//             if (sm.beacons[j].signals.length !== signalLength) {
-//                 print(sm.beacons[j].signals.length);
-//                 print(signalLength);
-//                 var room = db.rooms.findOne({_id: sm.room});
-//                 print("ERROR WITH SM " + sm._id + " from building " + room.building);
-//                 return;
-//             }
-//
-//             let beacon = undefined;
-//             if (!sm.beacons[j].name){
-//                 if (!sm.beacons[j]._id){
-//                     print("wtf");
-//                     return;
-//                 } else {
-//                     beacon = db.beacons.findOne({_id: sm.beacons[j]._id});
-//                 }
-//             } else {
-//                 beacon = db.beacons.findOne({name: sm.beacons[j].name});
-//             }
-//
-//             if (!beacon) {
-//                 let room = db.rooms.findOne({_id: sm.room});
-//                 db.beacons.insert({name: sm.beacons[j].name, building: room.building});
-//             }
-//             newSm.beacons.push({name: sm.beacons[j].name, signal: sm.beacons[j].signals[i]});
-//         }
-//
-//         print("inserted " + smInserted);
-//         smInserted++;
-//         db.signalmaps.insert(newSm);
-//     }
-// });
+signalmaps = db.signalmaps.find();
+let smInserted = 0;
+let smRemoved = 0;
+signalmaps.forEach(sm => {
+    let signalLength = sm.beacons[0].signals.length;
+    print("removed sm " + smRemoved);
+    smRemoved++;
+    // db.signalmaps.remove({_id: sm._id});
+
+
+    for (let i = 0; i < sm.beacons[0].signals.length; i++) {
+        var newSm = {
+            isActive: true,
+            room: sm.room,
+            beacons: []
+        }
+
+        for (let j = 0; j < sm.beacons.length; j++) {
+
+            if (sm.beacons[j].signals.length !== signalLength) {
+                print(sm.beacons[j].signals.length);
+                print(signalLength);
+                var room = db.rooms.findOne({_id: sm.room});
+                print("ERROR WITH SM " + sm._id + " from building " + room.building);
+                return;
+            }
+
+            let beacon = undefined;
+            if (!sm.beacons[j].name){
+                if (!sm.beacons[j]._id){
+                    print("wtf");
+                    return;
+                } else {
+                    beacon = db.beacons.findOne({_id: sm.beacons[j]._id});
+                }
+            } else {
+                beacon = db.beacons.findOne({name: sm.beacons[j].name});
+            }
+
+            if (!beacon) {
+                let room = db.rooms.findOne({_id: sm.room});
+                // db.beacons.insert({name: sm.beacons[j].name, building: room.building});
+            }
+            newSm.beacons.push({name: sm.beacons[j].name, signal: sm.beacons[j].signals[i]});
+        }
+
+        print("inserted " + smInserted);
+        smInserted++;
+        // db.signalmaps.insert(newSm);
+    }
+});
