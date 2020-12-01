796b5279c6261f95c4d48b8354df6563c2534319
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-12-01 09:08:50 +0100

    Added update_beacon_names script

--- scripts/update_beacon_id_to_name.js
+++ scripts/update_beacon_id_to_name.js
@@ -23,7 +23,7 @@ signalMaps.forEach(sm => {
     const beacons = []
     sm.beacons.forEach(b => {
         // print(b._id.toString());
-        if (b._id.toString() === "ObjectId(\"5db55d1256505b7106423e49\")"){
+        if (b._id.toString() === "ObjectId(\"5e37f440090d4f722ad1a21d\")"){
             print("whaat " + sm._id + " " + b._id);
         }
         if (b._id.toString() === "ObjectId(\"5e37f456090d4f722ad1a21e\")"){
